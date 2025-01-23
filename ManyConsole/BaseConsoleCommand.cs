b4033107a85a33eebb12bf0c30a3d9b971c9d89c
Mohsen Seifi <mohsen.seifi@sage.com>
2025-01-23 23:04:34 +0000

    Refactor

---
+++ ManyConsole/BaseConsoleCommand.cs
@@ -0,0 +1,118 @@
+﻿using ManyConsole.Internal;
+using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManyConsole
+{
+    public abstract class BaseConsoleCommand : ConsoleUtil
+    {
+        private static readonly string[] DefaultTraceCommandSkipProperties =
+        {
+            nameof(Command),
+            nameof(Aliases),
+            nameof(OneLineDescription),
+            nameof(LongDescription),
+            nameof(Options),
+            nameof(TraceCommandAfterParse),
+            nameof(TraceCommandSkipProperties),
+            nameof(RemainingArgumentsCountMin),
+            nameof(RemainingArgumentsCountMax),
+            nameof(RemainingArgumentsHelpText),
+            nameof(RequiredOptions)
+        };
+
+        public string Command { get; protected internal set; }
+        public List<string> Aliases { get; protected internal set; }
+        public string OneLineDescription { get; protected internal set; }
+        public string LongDescription { get; protected internal set; }
+        public OptionSet Options { get; protected set; }
+        public bool TraceCommandAfterParse { get; protected internal set; }
+        public ISet<string> TraceCommandSkipProperties { get; protected internal set; }
+        public int? RemainingArgumentsCountMin { get; protected internal set; }
+        public int? RemainingArgumentsCountMax { get; protected internal set; }
+        public string RemainingArgumentsHelpText { get; protected internal set; }
+        protected internal OptionSet OptionsHasd { get; set; }
+        protected internal List<RequiredOptionRecord> RequiredOptions { get; set; }
+
+        public BaseConsoleCommand()
+        {
+            OneLineDescription = "";
+            Options = new OptionSet();
+            TraceCommandAfterParse = true;
+            TraceCommandSkipProperties = new SortedSet<string>(DefaultTraceCommandSkipProperties, StringComparer.Ordinal);
+            RemainingArgumentsCountMax = 0;
+            RemainingArgumentsHelpText = "";
+            OptionsHasd = new OptionSet();
+            RequiredOptions = new List<RequiredOptionRecord>();
+        }
+
+        public virtual void CheckRequiredArguments()
+        {
+            var missingOptions = RequiredOptions
+                .Where(o => !o.WasIncluded).Select(o => o.Name).OrderBy(n => n).ToArray();
+
+            if (missingOptions.Any())
+            {
+                throw new ConsoleHelpAsException("Missing option: " + string.Join(", ", missingOptions));
+            }
+        }
+
+        public virtual int? OverrideAfterHandlingArgumentsBeforeRun(string[] remainingArguments)
+        {
+            return null;
+        }
+
+        public OptionSet GetActualOptions()
+        {
+            var result = new OptionSet();
+
+            foreach (var option in Options)
+                result.Add(option);
+
+            foreach (var option in OptionsHasd)
+                result.Add(option);
+
+            return result;
+        }
+
+        protected internal void SetAlias(string alias)
+        {
+            if (!String.IsNullOrEmpty(alias))
+            {
+                if (Aliases == null)
+                {
+                    Aliases = new List<string>();
+                }
+                Aliases.Add(alias);
+            }
+        }
+
+        protected internal void SetAdditionalArgumentsBetween(int? min, int? max, string helpText)
+        {
+            RemainingArgumentsCountMin = min;
+            RemainingArgumentsCountMax = max;
+            RemainingArgumentsHelpText = helpText;
+        }
+
+        protected internal void SetRequiredOption<T>(string prototype, string description, Action<T> action)
+        {
+            var requiredRecord = new RequiredOptionRecord();
+
+            var previousOptions = OptionsHasd.ToArray();
+
+            OptionsHasd.Add<T>(prototype, description, s =>
+            {
+                requiredRecord.WasIncluded = true;
+                action(s);
+            });
+
+            var newOption = OptionsHasd.Single(o => !previousOptions.Contains(o));
+
+            requiredRecord.Name = newOption.GetNames().OrderByDescending(n => n.Length).First();
+
+            RequiredOptions.Add(requiredRecord);
+        }
+    }
+}
---
+++ ManyConsole/IConsoleCommand.cs
@@ -0,0 +1,42 @@
+﻿using Mono.Options;
+using System;
+
+namespace ManyConsole
+{
+    internal interface IConsoleCommand<T>
+    {
+        T IsCommand(string command, string oneLineDescription = "");
+
+        T HasAlias(string alias);
+
+        T HasLongDescription(string longDescription);
+
+        T HasAdditionalArguments(int? count = 0, string helpText = "");
+
+        T HasAdditionalArgumentsBetween(int? min, int? max, string helpText = "");
+
+        T AllowsAnyAdditionalArguments(string helpText = "");
+
+        T SkipsCommandSummaryBeforeRunning();
+
+        T SkipsPropertyInCommandSummary(string propertyName);
+
+        T HasOption(string prototype, string description, Action<string> action);
+
+        T HasRequiredOption(string prototype, string description, Action<string> action);
+
+        T HasOption<TAction>(string prototype, string description, Action<TAction> action);
+
+        T HasRequiredOption<TAction>(string prototype, string description, Action<TAction> action);
+
+        T HasOption(string prototype, string description, OptionAction<string, string> action);
+
+        T HasOption<TKey, TValue>(string prototype, string description, OptionAction<TKey, TValue> action);
+
+        void CheckRequiredArguments();
+
+        int? OverrideAfterHandlingArgumentsBeforeRun(string[] remainingArguments);
+
+        OptionSet GetActualOptions();
+    }
+}
